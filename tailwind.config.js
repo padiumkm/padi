99c6944cc016788b0ce27bd611d0a03533dcc9ae
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2022-11-13 08:23:52 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        placeholder: "#949AB5",
+        gray: "#F1F3F7",
+        primary: "#0092AC",
+        secondary: "#F6F9FC",
+      },
+    },
+  },
+  plugins: [],
+};
